0e91b4f30efd7749cd1a8a6bfe3de45db16e2e8b
ggugga <113493380+ggugga@users.noreply.github.com>
2024-07-15 01:06:38 +0900

    secbpf_alist.c

---
+++ secbpf_alist.c
@@ -0,0 +1,78 @@
+// BPF를 통해 지정한 시스템 콜의 호출만을 허용하는 예제 코드
+// Name: secbpf_alist.c
+// Compile: gcc -o secbpf_alist secbpf_alist.c
+#include <fcntl.h>
+#include <linux/audit.h>
+#include <linux/filter.h>
+#include <linux/seccomp.h>
+#include <linux/unistd.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/prctl.h>
+#include <unistd.h>
+#define ALLOW_SYSCALL(name)                               \
+  BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_##name, 0, 1), \
+      BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_ALLOW)
+#define KILL_PROCESS BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL)
+#define syscall_nr (offsetof(struct seccomp_data, nr))
+#define arch_nr (offsetof(struct seccomp_data, arch))
+/* architecture x86_64 */
+#define ARCH_NR AUDIT_ARCH_X86_64
+int sandbox() {
+  struct sock_filter filter[] = {
+      /* 아키텍처 검사 */
+  /* X86_64라면 다음 코드로 분기하고 */
+  /* 다른 아키텍처라면 SECCOMP_RET_KILL을 반환하고 프로그램을 종료 */
+      BPF_STMT(BPF_LD + BPF_W + BPF_ABS, arch_nr),
+      BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, ARCH_NR, 1, 0),
+      BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
+      /* Get system call number. */
+      BPF_STMT(BPF_LD + BPF_W + BPF_ABS, syscall_nr),
+      /* 시스템 콜 검사 */
+  /* 호출된 시스템 콜의 번호를 저장 후 ALLOW_SYSCALL 매크로를 호출 */
+  /* 호출된 시스템 콜이 인자로 전달된 시스템 콜과 일치하는지 비교 */
+  /* 같다면  SECCOMP_RET_ALLOW를 반환, 다를시 kill 반환 */
+      ALLOW_SYSCALL(rt_sigreturn),
+      ALLOW_SYSCALL(open),
+      ALLOW_SYSCALL(openat),
+      ALLOW_SYSCALL(read),
+      ALLOW_SYSCALL(write),
+      ALLOW_SYSCALL(exit_group),
+      KILL_PROCESS,
+  };
+
+
+  
+  struct sock_fprog prog = {
+      .len = (unsigned short)(sizeof(filter) / sizeof(filter[0])),
+      .filter = filter,
+  };
+  if (prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0) == -1) {
+    perror("prctl(PR_SET_NO_NEW_PRIVS)\n");
+    return -1;
+  }
+  if (prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER, &prog) == -1) {
+    perror("Seccomp filter error\n");
+    return -1;
+  }
+  return 0;
+}
+void banned() { fork(); }
+int main(int argc, char* argv[]) {
+  char buf[256];
+  int fd;
+  memset(buf, 0, sizeof(buf));
+  sandbox();
+  if (argc < 2) {
+    banned();
+  }
+  fd = open("/bin/sh", O_RDONLY);
+  read(fd, buf, sizeof(buf) - 1);
+  write(1, buf, sizeof(buf));
+  return 0;
+}
+
+// $ ./secbpf_alistBad system call (core dumped)
+// $ ./secbpf_alist 1ELF> J@X?@8	@@@?888h?h? P?P?!
